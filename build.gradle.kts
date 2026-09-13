e5f3d8c119a229a107f56fa46864b7b7b54d2e0f
Cam-Zedd <73273345+Cam-Zedd@users.noreply.github.com>
2026-09-13 14:37:10 +0200

    Upgrade Android and Kotlin plugin versions
    Updated Android and Kotlin plugin versions.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,30 +1,4 @@
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
-}
-
-android {
-    namespace = "fr.zeddcara.portfoliotracker"
-    compileSdk = 35
-
-    defaultConfig {
-        applicationId = "fr.zeddcara.portfoliotracker"
-        minSdk = 26
-        targetSdk = 35
-        versionCode = 2
-        versionName = "1.1-v5.7"
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
-        }
-    }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-    kotlinOptions { jvmTarget = "17" }
+    id("com.android.application") version "8.7.3" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
 }
